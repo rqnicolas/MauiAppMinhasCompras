dba17b22f251b25d2935b2366d49273ee0e904c8
rqnicolas <nicolasqueirozmarques03407@gmail.com>
2025-03-24 00:03:52 -0300

    Atualização do código usando toolbaritem e await

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDataBaseHelper _db;
+
+        public static SQLiteDataBaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                            "banco_aqlite_compras.db3");
+                    _db = new SQLiteDataBaseHelper(path);
+                }
+                return _db;
+
+            }
+        }
         public App()
         {
             InitializeComponent();
